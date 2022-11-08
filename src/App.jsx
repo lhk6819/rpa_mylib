1569fa6f854ac71693f86041fc684c8021d41379
lhk6819 <lhk6819@gmail.com>
2022-11-08 21:44:50 +0900

    몇 가지 설정 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,57 @@
-import './App.css';
+import { Book } from './components/Book';
+import { Nav } from './components/Nav';
+// import { useData } from './hooks/data-context';
 
 function App() {
+  // const { data, addBook } = useData();
+
   return (
-    <>
-      <div className='App'></div>
-    </>
+    // <div className='bg-cyan-100x h-screen  w-full overflow-y-hidden overflow-x-scroll'>
+    //   <header>
+    //     <Nav />
+    //   </header>
+
+    //   {/* <Book
+    //             key={book.id}
+    //             book={book}
+    //             saveBook={saveBook}
+    //             removeBook={removeBook}
+    //           /> */}
+    //   <main>
+    //     <div className='flex items-start p-4'>
+    //       {data.books
+    //         .sort((a, b) =>
+    //           a.id === 0 ? Number.MAX_SAFE_INTEGER : a.id - b.id
+    //         )
+    //         .map((book) => (
+    //           <Book key={book.id} book={book} />
+    //         ))}
+    //       <div>
+    //         {data.books.find((book) => !book.id) ? (
+    //           ''
+    //         ) : (
+    //           <button
+    //             onClick={addBook}
+    //             className='mr-2 w-64 rounded-sm bg-cyan-400 px-5 py-1 font-medium text-white hover:bg-cyan-500'
+    //           >
+    //             + Add Book
+    //           </button>
+    //         )}
+    //       </div>
+    //     </div>
+    //   </main>
+
+    //   <footer className='text-center text-slate-500'>Project Fire</footer>
+    // </div>
+    <div>
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <Book />
+      </main>
+      <footer>Copyright Project Fire!</footer>
+    </div>
   );
 }
 
