32e35b58ec6eb43d51abdd3488c60aff4a529f36
lhk6819 <lhk6819@gmail.com>
2022-11-08 20:50:53 +0900

    copy tailwind CSS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,32 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
   },
   extends: [
-    'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
+    'airbnb',
   ],
-  overrides: [],
+  overrides: [
+  ],
   parserOptions: {
-    ecmaFeatures: { jsx: true },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
+  plugins: [
+    'react',
+  ],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off',
-    // 'react-hooks/rules-of-hooks': 'error', 일단은 주석처리 하겠습니다.
-    'react-hooks/exhaustive-deps': 'off',
-    'react/prop-types': 'off',
-    'prettier/prettier': 'error',
-    quotes: ['error', 'single', { allowTemplateLiterals: true }],
-  },
-  settings: {
-    react: { version: 'detect' }, // for react version warnning
   },
 };
